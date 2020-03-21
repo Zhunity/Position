4e35228366f1447a9b08beb200468e47f31e988e
lihua <poinseting@163.com>
2020-03-22 00:53:01 +0800

    GetSize

--- Assets/Position.cs
+++ Assets/Position.cs
@@ -11,21 +11,27 @@ public class Position : MonoBehaviour
         rect = GetComponent<RectTransform>();
     }
 
-    public Vector3 position;
-    public Vector2 anchoredPosition;
-    public Vector3 anchoredPosition3D;
-    public Vector3 localPosition;
-    
-  
-    public Vector2 sub;
-    public Vector2 guess;
+	public Vector3 position;
+	public Vector2 anchoredPosition;
+	public Vector3 anchoredPosition3D;
+	public Vector3 localPosition;
+	public Vector2 sub;
+	public Vector2 guess;
+
+
 	private Vector2 anchorSub;
     private Vector2 cAndPLocalSub;    // 父节点localPosition与子localPosition的插值
     private Vector2 cAndPAnchoredSub;    // 父节点localPosition与子localPosition的插值
 	private Vector2 parentPivot; // 因父节点pivot修改导致的子节点localPosition与anchoredPosition间的转换插值
-	public Vector2 selfPivot; // 因自己pivot修改导致的变化
+	private Vector2 selfPivot; // 因自己pivot修改导致的变化
 
-    private void Update()
+	private Vector2 RectSize;
+	private Vector2 offsetMax;
+	private Vector2 offsetMin;
+	private Vector2 ofssetToSize;
+	private Vector2 sizeDelta;
+
+	private void Update()
     {
         position = transform.position;
         anchoredPosition = rect.anchoredPosition;
@@ -46,27 +52,38 @@ public class Position : MonoBehaviour
         parentPivot = GetParentPivot();
 
 		selfPivot = new Vector2(
-			((rect.anchorMax.x - rect.anchorMin.x) * (0.5f - rect.pivot.x)) * rect.rect.size.x,
+			((rect.anchorMax.x - rect.anchorMin.x) * (0.5f - rect.pivot.x)) * GetSize(rect).x,
 			((rect.anchorMax.y - rect.anchorMin.y) * (0.5f - rect.pivot.y)) * rect.rect.size.y
 			);
 
 		guess = anchorSub + parentPivot + selfPivot;
-    }
+
+		RectSize = rect.rect.size;
+		offsetMax = rect.offsetMax;
+		offsetMin = rect.offsetMin;
+		ofssetToSize = GetSize(rect);
+		sizeDelta = rect.sizeDelta;
+	}
 
     private Vector2 GetParentPivot()
     {
         RectTransform parent = rect.parent as RectTransform;
-        cAndPLocalSub = new Vector2(
-          (0.5f - rect.pivot.x) * rect.rect.size.x - (0.5f - parent.pivot.x) * parent.rect.size.x,
-          (0.5f - rect.pivot.y) * rect.rect.size.y - (0.5f - parent.pivot.y) * parent.rect.size.y
-          );
+		cAndPLocalSub = new Vector2(
+			  (0.5f - rect.pivot.x) * GetSize(rect).x - (0.5f - parent.pivot.x) * GetSize(parent).x,
+				 (0.5f - rect.pivot.y) * rect.rect.size.y - (0.5f - parent.pivot.y) * parent.rect.size.y
+				 );
 
-        cAndPAnchoredSub = new Vector2(
-			(rect.pivot.x - 0.5f) * rect.rect.size.x,
-			(rect.pivot.y - 0.5f) * rect.rect.size.y
-			);
-        return cAndPAnchoredSub + cAndPLocalSub;
+		cAndPAnchoredSub = new Vector2(
+		 (rect.pivot.x - 0.5f) * GetSize(rect).x,
+		 (rect.pivot.y - 0.5f) * rect.rect.size.y
+		 );
+		return cAndPAnchoredSub + cAndPLocalSub;
     }
+
+	private Vector2 GetSize(RectTransform r)
+	{
+		return r.rect.size;// - rect.offsetMax + rect.offsetMin;
+	}
 
     void PositionAndLocalPosition()
     {
