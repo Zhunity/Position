68f6d1e33178579ffe31a1f04cdd2e5aa67d168e
lihua <poinseting@163.com>
2020-04-11 20:44:28 +0800

    添加local证明

---
+++ Assets/UILocalPosition/UILocalPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILocalPosition : MonoBehaviour
+{
+	public Transform Cube;
+	public Vector3 CubeLocalPosition;
+	public Vector3 ImageLocalPosition;
+	
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Cube.position = transform.position;
+		CubeLocalPosition = Cube.localPosition;
+		ImageLocalPosition = transform.localPosition;
+	}
+}
