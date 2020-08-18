503ac4832f27247b930b84471bf4d4113c2838d6
lihua <poinseting@163.com>
2020-08-18 23:48:02 +0800

    anchorposition证明

---
+++ Assets/Geometric/GeometricTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeometricTest : MonoBehaviour
+{
+
+	public RectTransform anchor;
+	public RectTransform sizeDelta;
+
+	private RectTransform self;
+
+	void Start()
+	{
+		self = GetComponent<RectTransform>();
+	}
+
+	public Vector3 anchorPosition;
+	public Vector3 reuslt;
+    // Update is called once per frame
+    void Update()
+    {
+		anchorPosition = self.anchoredPosition;
+
+
+	}
+}
